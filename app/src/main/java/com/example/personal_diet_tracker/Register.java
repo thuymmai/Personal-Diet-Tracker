a648d11891d441fb362c673ee4c43e57004c6016
thuymmai <thuymmai11@gmail.com>
2026-07-29 21:52:08 -0500

    Added input validation in Register.java

--- app/src/main/java/com/example/personal_diet_tracker/Register.java
+++ app/src/main/java/com/example/personal_diet_tracker/Register.java
@@ -3,6 +3,8 @@ package com.example.personal_diet_tracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -24,6 +26,21 @@ public class Register extends AppCompatActivity {
         backButton.setOnClickListener(view -> {
             Intent intent = new Intent(Register.this, RegisterAndLogin.class);
             startActivity(intent);
+        });
+
+        Button registerButton = findViewById(R.id.buttonRegisterReg);
+        registerButton.setOnClickListener(view -> {
+            EditText userName = findViewById(R.id.editUserNameReg);
+            String userNameText = userName.getText().toString();
+
+            EditText password = findViewById(R.id.editPasswordReg);
+            String passwordText = password.getText().toString();
+
+            if (userNameText.isEmpty() && passwordText.isEmpty()) {
+                Toast.makeText(this, "Please enter valid input", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Yay!", Toast.LENGTH_SHORT).show();
+            }
         });
 
     }
