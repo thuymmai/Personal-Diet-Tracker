ddabdf8c3a5b25f30f32614045dcea13920da3ec
thuymmai <thuymmai11@gmail.com>
2026-07-29 21:31:09 -0500

    Created Homepage.java and activity_homepage.xml

--- app/src/main/java/com/example/personal_diet_tracker/Register.java
+++ app/src/main/java/com/example/personal_diet_tracker/Register.java
@@ -1,6 +1,10 @@
 package com.example.personal_diet_tracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +20,15 @@ public class Register extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
+        ImageView backButton = findViewById(R.id.imageBackButton);
+        backButton.setOnClickListener(view -> {
+            Intent intent = new Intent(Register.this, RegisterAndLogin.class);
+            startActivity(intent);
+        });
+
+    }
+
+    public void onButtonClick(View view) {
+        Toast.makeText(this, "Back to Welcome Page", Toast.LENGTH_SHORT).show();
     }
 }
